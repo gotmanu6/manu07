fa503272c9c778abd4bc4cb03212c4cd8f1e55b3
Emanuelly <manu.presteslopes10@gmail.com>
2026-03-24 13:18:14 -0300

    model

---
+++ manu07/src/main/java/models/FornecedorModel.java
@@ -0,0 +1,51 @@
+package models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_fornecedor")
+public class FornecedorModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nomeFantasia;
+    @Column(name = "documento")
+    private String cnpj;
+    @Column(name = "fone")
+    private String contato;
+
+    public FornecedorModel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+}
