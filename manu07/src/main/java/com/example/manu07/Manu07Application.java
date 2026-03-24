c7653080bd11dbe0ac0544950ddb68e147cefee8
Emanuelly <manu.presteslopes10@gmail.com>
2026-03-24 13:17:09 -0300

    Initial commit

---
+++ manu07/src/main/java/com/example/manu07/Manu07Application.java
@@ -0,0 +1,13 @@
+package com.example.manu07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Manu07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Manu07Application.class, args);
+	}
+
+}
